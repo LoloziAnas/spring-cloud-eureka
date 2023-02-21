5edbbe6d8d5ce7a43782aab065584b1e1f3af8fb
@LoloziAnas <lolozianas1@gmail.com>
2023-02-21 17:47:50 +0100

    first commit

---
+++ registry-service/src/test/java/dev/anaslolozi/registryservice/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.anaslolozi.registryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
